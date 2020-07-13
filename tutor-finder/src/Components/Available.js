7e545f62ce31980b8ad7873151ec356402ffcd1e
Rafii Ramii <info.rafyramy@gmail.com>
2020-07-13 11:38:25 +0300

    added screen and options in the screen app section

--- tutor-finder/src/Components/Available.js
+++ tutor-finder/src/Components/Available.js
@@ -2,10 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import apple from "../Images/apple.png";
 import google from "../Images/google.png";
+import screen from "../Images/screenShot.png";
+import time from "../Images/time2.png";
+
 // css
 const AvailableContainer = styled.div`
   display: flex;
-  height: 90vh;
+  height: 100vh;
   padding: 2rem 10rem;
 
   .availableContents {
@@ -71,10 +74,89 @@ const AvailableContainer = styled.div`
       }
     }
   }
-  .chat {
-    border: 2px solid red;
-    margin-left: 9rem;
-    width: 30rem;
+
+  .chatScreen {
+    z-index: 8000;
+
+    .avalaibleScreen {
+      z-index: 800;
+
+      border: 13px solid black;
+      border-radius: 30px;
+      height: 560px;
+      margin-top: 3rem;
+      margin-left: 16rem;
+      img {
+        z-index: 800;
+        transform: rotate(15deg);
+        height: 560px;
+      }
+      img:after {
+        content: "";
+        display: block;
+        background-color: black;
+        position: absolute;
+        width: 100px;
+        height: 40px;
+        right: 12rem;
+      }
+    }
+
+    .informations {
+      background-color: white;
+      margin-top: -17rem;
+      margin-left: 2.5rem;
+      position: relative;
+      z-index: 900000000000000000 !important;
+      box-shadow: 1px 1px 12px 1px #888888;
+      width: 260px;
+      border-radius: 15px;
+      padding: 1rem 0.5rem;
+      padding-bottom: 6rem;
+      font-size: 0.8rem;
+      p {
+        font-weight: bolder;
+        margin-left: 1rem;
+        margin-top: 1rem;
+      }
+      input {
+        margin-left: 1rem;
+      }
+    }
+    .time {
+      margin-top: -23rem;
+      margin-left: 27rem;
+      box-shadow: 1px 1px 12px 1px #888888;
+      border-radius: 3px;
+      position: absolute;
+      background: #7fa99b;
+      width: 290px;
+      height: 60px;
+      display: flex;
+      flex-direction: row;
+      color: white;
+      padding-left: 0.5rem;
+      div {
+        margin-left: 0.4rem;
+      }
+      img {
+        background-color: #ffffff2e;
+        border-radius: 2px;
+        padding: 0.56rem;
+        margin-top: 1rem;
+        width: 14px;
+        height: 12px;
+      }
+      p {
+        font-size: 0.86rem;
+        font-weight: bolder;
+      }
+      h6 {
+        font-size: 0.76rem;
+
+        margin-top: -0.7rem;
+      }
+    }
   }
 `;
 
@@ -101,6 +183,38 @@ function Available() {
             <h6>
               GET IT ON <br /> <span className="bigText">Google Play</span>
             </h6>
+          </div>
+        </div>
+      </div>
+      <div className="chatScreen">
+        <div className="avalaibleScreen">
+          <img src={screen} />
+        </div>
+        <div className="informations">
+          <p>Meeting Places</p>
+          <div>
+            <input type="radio" name="home" />
+            Home
+            <input type="radio" name="home" />
+            Online
+            <input type="radio" name="home" />
+            Class
+          </div>
+          <div>
+            <p>Gender</p>
+            <div>
+              <input type="radio" name="home" />
+              Female
+              <input type="radio" name="home" />
+              Male
+            </div>
+          </div>
+        </div>
+        <div className="time">
+          <img src={time} />
+          <div>
+            <p>Meeting time</p>
+            <h6>this suppose to be the time you choose</h6>
           </div>
         </div>
       </div>
